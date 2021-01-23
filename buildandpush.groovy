6149bf71d6e81c9f927374e6d737862b0527dbd8
yassine b <yassine.boumajane@aiesec.net>
2021-01-23 14:53:36 +0100

    Adding Fix to Groovyfile

--- buildandpush.groovy
+++ buildandpush.groovy
@@ -8,8 +8,8 @@ pipeline {
 				script {
                     withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
                     sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
-					sh 'docker build -t lab/$BRANCH_NAME:$BUILD_NUMBER .'
-					sh 'docker push lab/$BRANCH_NAME:$BUILD_NUMBER'
+					sh 'docker build -t nimrodops/$BRANCH_NAME:$BUILD_NUMBER .'
+					sh 'docker push nimrodops/$BRANCH_NAME:$BUILD_NUMBER'
 				
 					}
 				}
