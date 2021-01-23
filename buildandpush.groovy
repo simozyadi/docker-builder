9d961c506cb60c338d5a95f1ad1430b7d003db1b
yassine b <yassine.boumajane@aiesec.net>
2021-01-23 14:46:22 +0100

    Removing Docker Hub Url

--- buildandpush.groovy
+++ buildandpush.groovy
@@ -7,7 +7,7 @@ pipeline {
 			steps{
 				script {
                     withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-                    sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD hub.docker.com'
+                    sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
 					sh 'docker build -t lab/$BRANCH_NAME:$BUILD_NUMBER .'
 					sh 'docker push lab/$BRANCH_NAME:$BUILD_NUMBER'
 				
